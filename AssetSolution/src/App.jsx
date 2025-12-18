ad640624007b9f8eb8ccf80ff8f3b32211dcf71d
AJFShuvo <ajfshuvo98@gmail.com>
2025-12-19 03:47:29 +0600

    React Lenis for smooth-scrooling(Navbar &App.jsx

--- AssetSolution/src/App.jsx
+++ AssetSolution/src/App.jsx
@@ -3,14 +3,28 @@ import About from './components/About';
 import Header from './components/Header';
 import Projects from './components/Projects';
 import Testimonials from './components/Testimonials';
+import ReactLenis from 'lenis/react'
 const App = () => {
   return (
+    <ReactLenis
+    root 
+      options={{
+        lerp:0.1,
+        duration:1.2, // smooth factor
+        orientation:'vertical',
+        gestureOrientation:'vertical',
+        smoothWheel:true,
+        wheelMultiplier:1,
+        smoothTouch:false,
+        touchMultiplier:2,
+      }}>
     <div className='w-full'>   
         <Header/>
         <About/>
         <Projects/>
         <Testimonials/>
     </div>
+    </ReactLenis>
   )
 }
 
